7d0c195117445cfca6194b6ee9c803af7421c27c
Francisco Villafáñez <110670253+FranVillafanez@users.noreply.github.com>
2022-08-25 12:59:30 -0300

    CAmbios para el defaío de arreglos

--- desafioVillafanez.js
+++ desafioVillafanez.js
@@ -1,3 +1,4 @@
+// Plantilla para crear un usuario
 class Usuario {
     constructor(nombre, apellido, cargo) {
         this.nombre = nombre,
@@ -5,12 +6,24 @@ class Usuario {
         this.cargo = cargo
     }
 }
-let empleadoNombre = prompt("Ingrese su nombre")
-let empleadoApellido = prompt("Ingrese su apellido")
-let empleadoCargo = prompt("Ingrese su cargo")
-let newUsuario = new Usuario (empleadoNombre, empleadoApellido, empleadoCargo)
-console.log(newUsuario)
-
+// Arreglo para registrar un usuario
+let registro = []
+// Cuántos registros desea
+let numeroRegistros = parseInt(prompt("Ingrese el número de registros que desea completar"))
+// Condicional
+for (let index = 0; index <= 10; index++) {
+    cargaUsuario()
+}
+// Datos para recolectar y crear un usuario
+function cargaUsuario(registro) {
+    let empleadoNombre = prompt("Ingrese su nombre")
+    let empleadoApellido = prompt("Ingrese su apellido")
+    let empleadoCargo = prompt("Ingrese su cargo")
+    let newUsuario = new Usuario (empleadoNombre, empleadoApellido, empleadoCargo)
+    registro.push(newUsuario)
+    console.log("Se agregó el usuario ", newUsuario)
+}
+// Cargar la fecha
 class Fecha {
     constructor (día, mes, horas) {
         this.dia = día,
@@ -18,14 +31,16 @@ class Fecha {
         this.horas = horas
     }
 }
-let diaFecha = prompt("Ingrese el día de la semana trabajado")
-let diaFechaNumero = parseInt(prompt("Ingrese el número del día"))
-let mesFecha = prompt("Ingresa el mes")
-let horasFecha = parseInt(prompt("Ingresa la cantidad de horas extra trabajadas"))
-let newHorasExtra = new Fecha (diaFecha, diaFechaNumero, mesFecha, horasFecha)
+// Arreglo para registrar la fecha
+let registroFecha = []
+// Datos para recolectar la fecha
+function name(registroFecha) {
+    let diaFecha = prompt("Ingrese el día de la semana trabajado")
+    let diaFechaNumero = parseInt(prompt("Ingrese el número del día"))
+    let mesFecha = prompt("Ingresa el mes")
+    let horasFecha = parseInt(prompt("Ingresa la cantidad de horas extra trabajadas"))
+    let newHorasExtra = new Fecha (diaFecha, diaFechaNumero, mesFecha, horasFecha)
+    registroFecha.pusch(registroFecha)
+    console.log("Se agregó la fecha ", newHorasExtra)
+}
 
-console.log(newHorasExtra)
-
-let ayudante = 300;
-let redactor = 500;
-let columnista = 1000;
