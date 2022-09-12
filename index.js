9b24030c2998d0ec700ccfe5ff0313e18b82e903
Francisco Villafáñez <110670253+FranVillafanez@users.noreply.github.com>
2022-09-12 09:33:37 -0300

    index cambio

---
+++ index.js
@@ -0,0 +1,32 @@
+// Interacción con el DOM
+const empleado = [{ nombre: "Fran", apellido: "Villafáñez", cargo: "Redactor" },
+                  {  nombre: "Juan", apellido: "Perez", cargo: "Administrativo" },
+                  {  nombre: "Pedro", apellido: "González", cargo: "Columnista"},
+                  {  nombre: "Jose", apellido: "Ruíz", cargo: "Redactor"}];
+                  function agregar(){
+                    let nombre = prompt('ingrese nombre')
+                    let apellido = prompt('ingrese apellido')
+                    let cargo = prompt('ingrese cargo')
+
+                    let newEmp = {
+                      nombre: nombre,
+                      apellido:apellido,
+                      cargo: cargo,
+                    }
+                    empleado.push(newEmp)
+                    let iniciar = prompt('quiere cargar otro dato?')
+                    if(iniciar === 'Si'){
+                      agregar()
+                    }else{
+                      for (const empleados of empleado) {
+                        let contenedor = document.createElement("div");
+                        contenedor.innerHTML = `<h2> Cargo: ${empleados.cargo}</h2>
+                                                <p>  Nombre: ${empleados.nombre}</p>
+                                                <p> Apellido: ${empleados.apellido}</p>`;
+                                                document.body.appendChild(contenedor);
+                                            }
+                    }
+                  }
+
+
+                  agregar()
